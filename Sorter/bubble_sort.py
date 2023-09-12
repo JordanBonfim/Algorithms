035ed6be3f38fac81bc191ace9c6afa4739512aa
Jordan <jordanduzabonfim@gmail.com>
2023-09-11 23:59:18 -0300

    chore: add bubble_sort py file

--- Sorter/bubble_sort.py
+++ Sorter/bubble_sort.py
@@ -3,20 +3,22 @@ import time
 ls = [10,9,8,7,6,5,4,3,2,1,0]
 ls_random = []
 for k in range(0,5000):
-    ls_random.append(random.randint(0,100))
+    ls_random.append(random.randint(0,10000))
 
 def bubble_sort(array):
+    comp=0
     print("Sorting: \n", array)
     start = time.time()
     for i in range(0, len(array)):
-        for j in range(0, len(array)-1):            
+        for j in range(0, len(array)-1):      
+            comp+=1      
             if(array[j]>array[j+1]):
                 temp=array[j+1]
                 array[j+1]=array[j]
                 array[j]=temp
     end = time.time()
     print("Sorted list: \n", array)
-    print(f"Sorted items: {len(ls_random)}  |    Elapsed time:{round(end-start,2)} seconds")
+    print(f"Sorted items: {len(ls_random)}   |   Elapsed time:{round(end-start,2)} seconds   |   {comp} comparisons")
     return ls
 
 bubble_sort(ls_random)
